9198764dc1d5ab0e2734bb1caaf9322440939f4b
Shulaev Ivan <mail@ntdservice.ru>
2023-08-16 23:11:10 +0300

    add poor register

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,12 +1,50 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useState, useRef } from "react";
 import { loginFormActive, registerFormActive } from "../../redux/slices/headerLoginFormSlice";
 
 const RegisterForm = () => {
+
+    const initialState = {
+        loginRef: useRef(null),
+        nameRef: useRef(null),
+        passwordRef: useRef(null),
+        confirmPasswordRef: useRef(null),
+        emailRef: useRef(null),
+        registerData: undefined,
+    };
+    const [registerFormState, setRegisterFormState] = useState(initialState);
     const dispatch = useDispatch();
 
     const closeFormHandler = () => {
         dispatch(registerFormActive(false));
+    };
+
+    const registerHandler = () => {
+        const registerData = {
+            login: registerFormState.loginRef.current.value,
+            userName: registerFormState.nameRef.current.value,
+            userPassword: registerFormState.passwordRef.current.value,
+            email: registerFormState.emailRef.current.value
+        }
+
+        const  fetchFunc = async () => {
+            await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user/register/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(registerData)
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                dispatch(registerFormActive(false));
+                dispatch(loginFormActive(false));
+            })
+        };
+
+        fetchFunc();
     };
     
     return (
@@ -15,18 +53,18 @@ const RegisterForm = () => {
                 <span className="register-form-close-btn" onClick={closeFormHandler}></span>
                 <form className="register-form-header">
                     <label htmlFor="register-from-header-login-input">Login</label>
-                    <input type="text" />
+                    <input ref={registerFormState.loginRef} type="text" />
                     <label htmlFor="register-from-header-email-input">Name</label>
-                    <input type="text" />
+                    <input ref={registerFormState.nameRef} type="text" />
                     <label htmlFor="register-from-header-password-input">Password</label>
-                    <input type="password" />
+                    <input ref={registerFormState.passwordRef} type="password" />
                     <label htmlFor="register-from-header-password-repeat-input">Password Repeat</label>
-                    <input type="password" />
+                    <input ref={registerFormState.confirmPasswordRef} type="password" />
                     <label htmlFor="register-from-header-email-input">Email</label>
-                    <input type="text" />
+                    <input ref={registerFormState.emailRef} type="text" />
                 </form>
                 <div className="register-from-login-btn-wrap">
-                    <button>Register</button>
+                    <button onClick={registerHandler}>Register</button>
                 </div>
             </div>
         </React.Fragment>
