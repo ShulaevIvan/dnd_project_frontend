46d31915b387195c7e2807a24c3dbaf9535f0150
shulaev ivan <mail@ntdservice.ru>
2023-12-24 22:30:24 +0300

    add attack tests

--- src/components/CharacterStepsPreviewTests/CharacterStepsPreviewTests.jsx
+++ src/components/CharacterStepsPreviewTests/CharacterStepsPreviewTests.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import CharacterStepsStatTest from "./CharacterStepsStatTest";
 import CharacterStepsAbilityTest from "./CharacterStepsAbilityTest";
+import CharacterStepsAttackTest from "./CharacterStepsAttackTest";
 
 const CharacterStepsPreviewTests = () => {
     return (
         <React.Fragment>
             <CharacterStepsStatTest />
             <CharacterStepsAbilityTest />
+            <CharacterStepsAttackTest />
+            
         </React.Fragment>
     )
 };
