316fd1b94d922127942d534ef2b98db38f694cb1
shulaev ivan <mail@ntdservice.ru>
2024-02-11 22:18:21 +0300

    add some fiexes

--- src/components/UsersAccount/UserCharacterInventory.jsx
+++ src/components/UsersAccount/UserCharacterInventory.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const UserCharacterPreviewInventory = () => {
+    const selectedCharacter = useSelector((state) => state.userData.previewCharacter.previewCharacterSelected);
+    
     return (
         <React.Fragment>
             <div className="preview-character-inventory-wrap">
@@ -11,57 +14,48 @@ const UserCharacterPreviewInventory = () => {
                         <div className="preview-character-inventory-money-gold">
                             <span className="character-inventory-coin-title">Зол</span>
                             <span className="character-inventory-coin-icon"></span>
-                            <span className="character-inventory-coin-title">100</span>
+                            <span className="character-inventory-coin-title">{selectedCharacter ? selectedCharacter.inventory.inventoryGold.gold : null}</span>
                         </div>
                         <div className="preview-character-inventory-money-silver">
                         <span className="character-inventory-coin-title">Сер</span>
                             <span className="character-inventory-coin-icon"></span>
-                            <span className="character-inventory-coin-title">100</span>
+                            <span className="character-inventory-coin-title">{selectedCharacter ? selectedCharacter.inventory.inventoryGold.silver : null}</span>
                         </div>
                         <div className="preview-character-inventory-money-bronze">
                             <span className="character-inventory-coin-title">Брон</span>
                             <span className="character-inventory-coin-icon"></span>
-                            <span className="character-inventory-coin-title">100</span>
+                            <span className="character-inventory-coin-title">{selectedCharacter ? selectedCharacter.inventory.inventoryGold.bronze : null}</span>
                         </div>
                     </div>
                 </div>
                 <div className="preview-character-inventory-main-row">
-                    <div className="preview-character-inventory-item">
-                        <div className="preview-character-inventory-item-title">Title</div>
-                        <div className="preview-character-inventory-image-wrap">
-                            <img src="/static/media/demo.630922c5cb9e25da873b.jpg" alt=""/>
-                        </div>
-                        <div className="preview-character-inventory-item-controls-row">
-                            <div className="inventory-info-item-btn-wrap">
-                                <span className="inventory-info-icon-btn"></span>
-                            </div>
-                            <div className="inventory-send-item-btn-wrap">
-                                <span className="inventory-send-icon-btn"></span>
-                            </div>
-                            <div className="inventory-remove-item-btn-wrap">
-                                <span className="inventory-remove-icon-btn"></span>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div className="preview-character-inventory-item">
-                        <div className="preview-character-inventory-item-title">Title</div>
-                        <div className="preview-character-inventory-image-wrap">
-                            <img src="/static/media/demo.630922c5cb9e25da873b.jpg" alt=""/>
-                        </div>
-                        <div className="preview-character-inventory-item-controls-row">
-                            <div className="inventory-info-item-btn-wrap">
-                                <span className="inventory-info-icon-btn"></span>
-                            </div>
-                            <div className="inventory-send-item-btn-wrap">
-                                <span className="inventory-send-icon-btn"></span>
-                            </div>
-                            <div className="inventory-remove-item-btn-wrap">
-                                <span className="inventory-remove-icon-btn"></span>
-                            </div>
-                        </div>
-                    </div>
-                    
+                    {selectedCharacter.inventory ? selectedCharacter.inventory.inventoryItems.map((inventoryItem) => {
+                        console.log(inventoryItem)
+                        return (
+                            <React.Fragment key={Math.random()}>
+                                <div className="preview-character-inventory-item">
+                                    <div className="preview-character-inventory-item-quantity">{`${inventoryItem.quantity} шт`}</div>
+                                        <div className="preview-character-inventory-item-title">
+                                            {inventoryItem.item.length > 0 ? inventoryItem.item[0]['name'] : null}
+                                        </div>
+                                        <div className="preview-character-inventory-image-wrap">
+                                            <img src="/static/media/demo.630922c5cb9e25da873b.jpg" alt=""/>
+                                        </div>
+                                        <div className="preview-character-inventory-item-controls-row">
+                                            <div className="inventory-info-item-btn-wrap">
+                                                <span className="inventory-info-icon-btn"></span>
+                                            </div>
+                                        <div className="inventory-send-item-btn-wrap">
+                                            <span className="inventory-send-icon-btn"></span>
+                                        </div>
+                                        <div className="inventory-remove-item-btn-wrap">
+                                            <span className="inventory-remove-icon-btn"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </React.Fragment>
+                        )
+                    }) : null}
                 </div>
             </div>
         </React.Fragment>
