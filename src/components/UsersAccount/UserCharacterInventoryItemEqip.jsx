abbb77cf0526404127c1df3b70bf74a338121d90
shulaev ivan <mail@ntdservice.ru>
2024-04-13 22:28:40 +0300

    add some fixes

--- src/components/UsersAccount/UserCharacterInventoryItemEqip.jsx
+++ src/components/UsersAccount/UserCharacterInventoryItemEqip.jsx
@@ -24,6 +24,8 @@ const UserCharacterInventoryItemEqip = () => {
         console.log(itemObj)
     };
 
+    
+
     return (
         <React.Fragment>
             <div className="preview-character-eqip-main-row">
@@ -114,7 +116,54 @@ const UserCharacterInventoryItemEqip = () => {
                         </div>
                     </div>
                 : null}
-                
+                <div className="character-item-eqip-popup-wrap">
+                    <div className="character-item-eqip-title">Head Slot</div>
+                    <div className="character-item-eqip-close-btn-wrap"></div>
+                    <div className="character-item-eqip-body-wrap">
+                        <div className="character-item-equipped-wrap">
+                            <div className="character-item-equipped">
+                                <div className="character-item-equipped-title">Gold helm</div>
+                                <div className="character-item-equipped-row">
+                                    <div className="character-item-equipped-params-wrap">
+                                        <div className="character-item-equipped-params">
+                                            <ul>
+                                                <li>params 1</li>
+                                                <li>params 2</li>
+                                                <li>params 3</li>
+                                                <li>params 4</li>
+                                                <li>params 5</li>
+                                                <li>params 6</li>
+                                                <li>params 7</li>
+                                                <li>params 8</li>
+                                                <li>params 9</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div className="character-item-equipped-image-wrap">
+                                        <div className="character-item-equipped-image">
+                                            <img src="#" alt="img"/>
+                                        </div>
+                                        <div className="character-item-equipped-description">
+                                            <p>description</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="character-inventory-items-wrap">
+                            <div className="character-inventory-items-title">Inventory items by slot</div>
+                            <select id="pet-select">
+                                <option value="">--Please choose an option--</option>
+                                <option value="dog">Dog</option>
+                                <option value="cat">Cat</option>
+                                <option value="hamster">Hamster</option>
+                                <option value="parrot">Parrot</option>
+                                <option value="spider">Spider</option>
+                                <option value="goldfish">Goldfish</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )
